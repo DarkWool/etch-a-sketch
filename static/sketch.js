8b11adb68a59491936f1fdee4babf8bb8223e7cc
DarkWool <cooper27@live.com.mx>
2021-10-30 19:56:12 -0500

    Added functions to create, clean and reset the grid to a white color

--- static/sketch.js
+++ static/sketch.js
@@ -1,94 +1,39 @@
-// ----- Floats method:
+let sketch = document.getElementById('sketch');
+let children = sketch.children;
 
-// let container = document.getElementById('container');
-
-// let multiple = 1;
-
-// Create 16 divs for the page.
-// for (let i = 0; i < 256; i++) {
-//     const div = document.createElement('div');
-    
-//     if (i === multiple) {
-//         div.classList.add('f-lineBreak');
-//         multiple += 16;
-//     }
-
-//     container.append(div.cloneNode());
-// }
-
-// ----- inline-block method:
-/*
-let container = document.getElementById('container');
-
-const div = document.createElement('div');
-div.classList.add('ib');
-
-let n = 15;
-for (let i = 0; i < 256; i++) {
-    if (i === n) {
-        n += 16;
-    }
-    
-    container.append(div.cloneNode());
-}
-*/
-
-
-// 4. CSS GRID.
-// let container = document.getElementById('container');
-// for (let i = 0; i < 256; i++) {
-    // let div = document.createElement('div');
-    // div.addEventListener('mouseover', (e) => {
-        // e.target.style.backgroundColor = random.value;
-    // })
-    // container.append(div);
-// }
-// 
-// 
-// while (children) {
-    // }
-    //     children[0].remove();
-    // Array.from(children).forEach(child => child.remove());
-    // container.innerHTML = '';
-
-function createGrid(e) {
-    cleanGrid();
-
-    console.log(e);
-
-    container.style.cssText = `grid-template-columns: repeat(${range.value}, 1fr); grid-template-rows: repeat(${range.value}, 1fr);`;
-
-    let totalDivs = range.value * range.value;
-
-    for (let i = 0; i < totalDivs; i++) {
-        let newDiv = document.createElement('div');
-
-        newDiv.addEventListener('mouseover', (e) => {
-            e.target.style.backgroundColor = colorPicker.value;
-        })
-
-        container.append(newDiv);
-    }
-}
-
-// Reset the grid to blank.
-let children = container.children;
-
+// Reset the grid to blank squares.
 let resetButton = document.getElementById('reset');
-resetButton.addEventListener('click', reset);
+resetButton.addEventListener('click', resetGrid);
 
+function resetGrid() {
+	for (let child of children) {
+		child.removeAttribute('style', '');
+	}
+}
 
 function cleanGrid() {
-    container.innerHTML = '';
+	sketch.innerHTML = '';
 }
 
-function reset() {
-    for (let child of children) {
-        child.removeAttribute('style', '');
-    }
+// Change the Grid/Sketch size.
+let gridSize = document.getElementById('grid-size');
+gridSize.addEventListener('change', createGrid);
+
+function createGrid(e) {
+	cleanGrid();
+
+	sketch.style.cssText = `grid-template-columns: repeat(${gridSize.value}, 1fr); grid-template-rows: repeat(${gridSize.value}, 1fr);`;
+
+	let totalDivs = gridSize.value * gridSize.value;
+
+	for (let i = 0; i < totalDivs; i++) {
+		let newDiv = document.createElement('div');
+		newDiv.classList.add('showGrid');
+		newDiv.addEventListener('mouseover', (e) => {
+			console.log(e);
+		});
+		sketch.append(newDiv);
+	}
 }
 
-let colorPicker = document.getElementById('picker');
-
-let range = document.getElementById('grid-size');
-range.addEventListener('change', createGrid);
+createGrid();
